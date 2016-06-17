ec64cecc44ee486496c037305503900f3cdb1b57
Tucker Schoos <TuckerSchoos@gmail.com>
2016-06-17 17:39:00 -0400

    update

---
+++ week02/FizzBuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(n):
+    for i in range(1,n):
+        if i%15 == 0:
+            print 'FizzBuzz'
+        elif i%3 == 0:
+            print 'Fizz'
+        elif i%5 == 0:
+            print 'Buzz'
+        else:
+            print i
+    return
+
+
+Answer = int(raw_input("Pick a number: "))
+fizzbuzz(Answer)
